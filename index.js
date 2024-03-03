989bf35e7045da6b3d97a3b9f945342f812b3de6
springtrap10252 <67726548+springtrap10252@users.noreply.github.com>
2024-03-03 18:06:37 +0800

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Listening to Springfall Airlines","Watching Springfall Airlines"];
 
 
 let currentIndex = 0;
